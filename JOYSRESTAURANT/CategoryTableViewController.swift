ce8d14661a0522a767a5bd01c9af70aeb17372de
Joy Best <fasterthanmomentum@gmail.com>
2019-10-26 14:00:44 -0400

    up to Part Six

--- JOYSRESTAURANT/CategoryTableViewController.swift
+++ JOYSRESTAURANT/CategoryTableViewController.swift
@@ -9,9 +9,16 @@
 import UIKit
 
 class CategoryTableViewController: UITableViewController {
+    let menuController = MenuController()
+    var categories = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        menuController.fetchCategories{ (categories) in
+            if let categories = categories {
+                self.updateUI(with: categories)
+            }
+        }
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -19,28 +26,45 @@ class CategoryTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+    func updateUI(with categories: [String]) {
+        DispatchQueue.main.async {
+            self.categories = categories
+            self.tableView.reloadData()
+        }
+    }
 
+
+    
+    
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return categories.count
     }
 
-    /*
+    func configure(_ cell: UITableViewCell, forItemAt indexPath: IndexPath) {
+          let categoryString = categories[indexPath.row]
+          cell.textLabel?.text = categoryString.capitalized
+      }
+      
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCellIdentifier", for: indexPath)
+        configure(cell, forItemAt: indexPath)
         // Configure the cell...
 
         return cell
     }
-    */
+    
+  
 
     /*
     // Override to support conditional editing of the table view.
@@ -86,5 +110,14 @@ class CategoryTableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "MenuSegue"
+        {
+            let menuTableViewController = segue.destination as!
+            MenuTableViewController
+            let index = tableView.indexPathForSelectedRow!.row
+            menuTableViewController.category = categories [index]
+        }    }
+    
+    
 }
